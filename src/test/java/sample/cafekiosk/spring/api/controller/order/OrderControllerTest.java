4c99d5718f93acf45352797dd1b02d01b1d9b6ea
fad <ojy9406@naver.com>
2025-09-26 16:13:20 +0900

    OrderController 테스트 코드 작성

---
+++ src/test/java/sample/cafekiosk/spring/api/controller/order/OrderControllerTest.java
@@ -0,0 +1,77 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.OrderService;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private OrderService orderService;
+
+    @Test
+    @DisplayName("신규 주문을 등록한다.")
+    void createOrder() throws Exception {
+        // given
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "002"))
+                .build();
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/v1/orders/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.name()))
+                .andExpect(jsonPath("$.message").value("OK"))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @Test
+    @DisplayName("신규 주문시 상품 번호는 1개 이상 넣어야 한다.")
+    void createProductWithoutEmptyProductNumbers() throws Exception {
+        // given
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of())
+                .build();
+
+        // when, then
+        mockMvc.perform(
+                        post("/api/v1/orders/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.name()))
+                .andExpect(jsonPath("$.message").value("상품 번호는 필수 입력 항목입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+}
